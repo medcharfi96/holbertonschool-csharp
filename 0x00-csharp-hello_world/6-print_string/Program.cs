c58982d0d9069289df181a57ce54c925533921ab
medcharfi96 <1389@holbertonschool.com>
2021-10-26 15:59:12 +0100

    add

---
+++ 0x00-csharp-hello_world/6-print_string/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _6_print_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Holberton School";
+			Console.WriteLine("{0}{0}{0}",str);
+			Console.WriteLine("{0}",str.Substring(0,9));
+        }
+    }
+}
