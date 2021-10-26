91e7087100bc2707e93306bac0e1c848e0dc4c9d
medcharfi96 <1389@holbertonschool.com>
2021-10-26 16:18:11 +0100

    add

---
+++ 0x00-csharp-hello_world/9-print_specifiers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _9_print_specifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			double percent = .7553;
+			double currency = 98765.4321;
+			Console.WriteLine("percent: {0}", percent*100);
+			Console.WriteLine("currency: ${0:N2}",currency);
+
+        }
+    }
+}
