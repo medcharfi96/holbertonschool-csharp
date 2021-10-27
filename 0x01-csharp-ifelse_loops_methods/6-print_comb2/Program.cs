f974b725aab047e2331316613ebc41cec94cbb29
medcharfi96 <1389@holbertonschool.com>
2021-10-27 12:31:14 +0100

    add

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<99; i++)
+			{
+				int zeb= i%10;
+				int acharat = i/10;
+				int test = 0;
+				test = (zeb * 10) + acharat;
+				if(zeb == acharat)
+					continue;
+				else if(test < i)
+					continue;
+				else
+					Console.Write("{0} ", i);
+			}
+        }
+    }
+}
