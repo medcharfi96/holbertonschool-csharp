68e953e8e636b00b0b31e66232982d7407945ab4
medcharfi96 <1389@holbertonschool.com>
2021-10-27 12:10:00 +0100

    add

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i<99; i++)
+			{
+				Console.Write("{0:00}, ",i);
+			}
+			Console.Write("{0:00} ",99);
+        }
+    }
+}
