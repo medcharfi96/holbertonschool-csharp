85b3bac541ef6b0a3073f4bee258083bd53dcc52
medcharfi96 <1389@holbertonschool.com>
2021-10-27 11:54:05 +0100

    add

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			for(char i ='a' ; i <= 'z'; i++)
+			{
+				if( i == 'e' || i == 'q')
+					continue;
+				Console.Write(i);
+			}
+		}
+    }
+}
