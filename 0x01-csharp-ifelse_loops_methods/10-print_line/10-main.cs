bd40a196907ae2e5e8ab87971964fe760cbe38e1
medcharfi96 <1389@holbertonschool.com>
2021-10-28 17:31:52 +0100

    add

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line.PrintLine(3);
+       		Line.PrintLine(0);
+        	Line.PrintLine(12);
+        	Line.PrintLine(-98);
+        }
+    }
+}
---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+		if(length <= 0)
+		{
+			Console.WriteLine("\n");
+		}
+		else
+		{
+			for(int i = 0 ; i< length; i++ )
+			{
+				Console.Write("_");
+			}
+		}
+	}
+}
